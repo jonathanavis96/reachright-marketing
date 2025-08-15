5f05e34e71848e49b03850f1c48a8ab1379ed472
jonathanavis96 <jonathanavis96@gmail.com>
2025-08-15 13:54:22 +0200

    Update Navigation.tsx

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -25,7 +25,7 @@ const Navigation = () => {
           {/* Logo */}
           <Link to="/" className="flex items-center space-x-2">
             <img
-              src="/assets/img/branding/android-chrome-192x192.png"
+              src="/reachright-marketing/assets/img/branding/android-chrome-192x192.png"
               alt="ReachRight Marketing"
               width={40}
               height={40}
