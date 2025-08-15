dacda63244099d7d57dd8776a3dd209e490f8c05
jonathanavis96 <jonathanavis96@gmail.com>
2025-08-15 15:44:48 +0200

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,19 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// src/main.tsx
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App";
+import "./index.css";
 
-createRoot(document.getElementById("root")!).render(<App />);
+const rootEl = document.getElementById("root");
+if (!rootEl) {
+  throw new Error('Root element "#root" not found');
+}
+
+createRoot(rootEl).render(
+  <React.StrictMode>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
