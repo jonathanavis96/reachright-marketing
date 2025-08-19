a4f3df592c92fdc84f62b2655c790091d5e68db2
jonathanavis96 <jonathanavis96@gmail.com>
2025-08-19 11:29:02 +0200

    Update App.tsx
    <BrowserRouter basename="/reachright-marketing">
    updated to 
    <BrowserRouter basename="/">

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter basename="/reachright-marketing">
+      <BrowserRouter basename="/">
         <Routes>
           <Route path="/" element={<Index />} />
           <Route path="/about" element={<About />} />
